126b78f4ed04e9007fab6c57be95c36c46f9d66c
Wesley Crowe <wesleycrowe999@gmail.com>
2025-06-30 23:32:58 -0700

    added credits menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,12 @@ using UnityEngine.UI;
 public class LevelSelect : MonoBehaviour
 {
     public Button[] levelButtons;
+    public Button creditsButton;
+    public Button menuButton;
     public Button exitButton; // Reference for the Exit button
+
+    public GameObject LevelMenu;
+    public GameObject CreditsMenu;
 
     void Start()
     {
@@ -16,13 +21,34 @@ public class LevelSelect : MonoBehaviour
             levelButtons[i].onClick.AddListener(() => LoadLevel(index));
         }
 
+        // Add the listener for the credits button
+        creditsButton.onClick.AddListener(Credits);
+        menuButton.onClick.AddListener(MenuReturn);
+
         // Add the listener for the exit button
         exitButton.onClick.AddListener(ExitGame);
+
+
+        LevelMenu = GameObject.Find("Level Select Panel");
+        CreditsMenu = GameObject.Find("Credits Panel");
+        CreditsMenu.SetActive(false);
     }
 
     void LoadLevel(int levelIndex)
     {
         SceneManager.LoadScene("Room_" + levelIndex);
+    }
+
+    void Credits()
+    {
+        LevelMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    void MenuReturn()
+    {
+        LevelMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
     }
 
     void ExitGame()
